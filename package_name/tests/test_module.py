79c168f85c00a835b690fbac71a9f379c574f74e
Scott Lowe <scott.code.lowe@gmail.com>
2016-01-11 20:26:38 +0000

    Refactor and extend cubic_rectification tests

--- package_name/tests/test_module.py
+++ package_name/tests/test_module.py
@@ -1,4 +1,9 @@
+"""
+Tests for module in package_name.
+"""
 import unittest
+import math
+
 import numpy as np
 
 from .base_test import BaseTestCase
@@ -9,16 +14,60 @@ from ..module import cubic_rectification
 
 class TestCubicRectification(BaseTestCase):
 
-    def test_scalar(self):
-        self.assertEqual(8, cubic_rectification(2))
-        self.assertEqual(0, cubic_rectification(-2))
-        self.assertEqual(27, cubic_rectification(3))
+    """
+    Tests for the cubic_rectification function.
+    """
+
+    def test_int(self):
+        '''
+        Test with integer inputs.
+        '''
+        self.assertEquals(cubic_rectification(2), 8)
+        self.assertEquals(cubic_rectification(-2), 0)
+        self.assertEquals(cubic_rectification(3), 27)
+
+    def test_float(self):
+        '''
+        Test with float inputs.
+        '''
+        # Need to use assert_allclose due to the real possibility of a
+        # floating point inaccuracy.
+        self.assert_allclose(cubic_rectification(1.2), 1.728)
+        self.assert_allclose(cubic_rectification(-1.2), 0)
 
     def test_array(self):
-        self.assert_equal(np.array(27), cubic_rectification(np.array(3)))
-        self.assert_equal(np.array([0, 8, 0]),
-                          cubic_rectification(np.array([0, 2, -2])))
+        '''
+        Test with numpy array inputs.
+        '''
+        # Test with empty array
+        self.assert_equal(cubic_rectification(np.array([])), np.array([]))
+        # Test with singleton array
+        self.assert_equal(cubic_rectification(np.array(3)), np.array(27))
+        # Test with vector
+        self.assert_equal(cubic_rectification(np.array([0, 2, -2])),
+                          np.array([0, 8, 0]))
+        # Test with arange input
+        x = np.arange(-3, 4)
+        actual = cubic_rectification(x)
+        desired = np.array([0, 0, 0, 0, 1, 8, 27])
+        self.assert_allclose(actual, desired)
 
     @unittest.expectedFailure
-    def test_skip_success(self):
-        self.assert_equal(float('nan'), (float('nan')))
+    def test_nan_skipped(self):
+        '''
+        Test for NaN input with invalid comparison methods.
+        '''
+        # We can't use the standard assertEquals for comparing two NaNs
+        self.assertEquals(cubic_rectification(float('nan')), float('nan'))
+        self.assertEquals(cubic_rectification(np.nan), np.nan)
+
+    def test_nan(self):
+        '''
+        Test for NaN input with valid comparison methods.
+        '''
+        # Can use the assert_equal from numpy.testing to compare NaNs
+        self.assert_equal(cubic_rectification(float('nan')), float('nan'))
+        self.assert_equal(cubic_rectification(np.nan), np.nan)
+        # Or we can use an isnan function from either math or numpy
+        self.assertTrue(math.isnan(cubic_rectification(float('nan'))))
+        self.assertTrue(np.isnan(cubic_rectification(np.nan)))

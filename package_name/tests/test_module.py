b3159b18420aadf64bb57b1a760c3768186defdf
Scott Lowe <scott.code.lowe@gmail.com>
2016-01-18 23:03:35 +0000

    Standardize docstring quotes. Turn docstrings into one-line.

--- package_name/tests/test_module.py
+++ package_name/tests/test_module.py
@@ -1,6 +1,7 @@
-"""
+'''
 Tests for module in package_name.
-"""
+'''
+
 import unittest
 import math
 
@@ -13,32 +14,25 @@ from ..module import cubic_rectification
 
 
 class TestCubicRectification(BaseTestCase):
-
-    """
+    '''
     Tests for the cubic_rectification function.
-    """
+    '''
 
     def test_int(self):
-        '''
-        Test with integer inputs.
-        '''
+        '''Test with integer inputs.'''
         self.assertEquals(cubic_rectification(2), 8)
         self.assertEquals(cubic_rectification(-2), 0)
         self.assertEquals(cubic_rectification(3), 27)
 
     def test_float(self):
-        '''
-        Test with float inputs.
-        '''
+        '''Test with float inputs.'''
         # Need to use assert_allclose due to the real possibility of a
         # floating point inaccuracy.
         self.assert_allclose(cubic_rectification(1.2), 1.728)
         self.assert_allclose(cubic_rectification(-1.2), 0)
 
     def test_array(self):
-        '''
-        Test with numpy array inputs.
-        '''
+        '''Test with numpy array inputs.'''
         # Test with empty array
         self.assert_equal(cubic_rectification(np.array([])), np.array([]))
         # Test with singleton array
@@ -54,17 +48,13 @@ class TestCubicRectification(BaseTestCase):
 
     @unittest.expectedFailure
     def test_nan_skipped(self):
-        '''
-        Test for NaN input with invalid comparison methods.
-        '''
+        '''Test for NaN input with invalid comparison methods.'''
         # We can't use the standard assertEquals for comparing two NaNs
         self.assertEquals(cubic_rectification(float('nan')), float('nan'))
         self.assertEquals(cubic_rectification(np.nan), np.nan)
 
     def test_nan(self):
-        '''
-        Test for NaN input with valid comparison methods.
-        '''
+        '''Test for NaN input with valid comparison methods.'''
         # Can use the assert_equal from numpy.testing to compare NaNs
         self.assert_equal(cubic_rectification(float('nan')), float('nan'))
         self.assert_equal(cubic_rectification(np.nan), np.nan)

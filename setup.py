4081a0de3ae985a4a6939e7e830fb461d23ccd35
Scott Lowe <scott.code.lowe@gmail.com>
2021-06-02 11:47:08 +0100

    STY: Change commented out strings to be black-style

--- setup.py
+++ setup.py
@@ -158,7 +158,7 @@ setup(
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     package_dir={meta["name"]: os.path.join(".", meta["path"])},
     # If any package contains *.txt or *.rst files, include them:
-    # package_data={'': ['*.txt', '*.rst'],}
+    # package_data={"": ["*.txt", "*.rst"],}
     install_requires=install_requires,
     extras_require=extras_require,
     # Metadata to display on PyPI

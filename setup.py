2e55c13813e7d47d74866b4b187b24b8985c29be
Scott Lowe <scott.code.lowe@gmail.com>
2021-06-11 10:38:59 +0100

    MNT: Add py2.7 and py3.5 Trove classifiers

--- setup.py
+++ setup.py
@@ -174,7 +174,10 @@ setup(
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         "Natural Language :: English",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",

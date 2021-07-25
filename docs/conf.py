20c82a9a902b310c3097cb8d86b6c9636a2b72a1
Scott Lowe <scott.code.lowe@gmail.com>
2021-07-25 23:17:47 +0100

    DOC: Add markdown documentation support using MyST

--- docs/conf.py
+++ docs/conf.py
@@ -103,6 +103,7 @@ def setup(app):
 # extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
 extensions = [
+    "myst_parser",  # Support markdown
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
